eaa38e9e9d6a7f6be0ae49491181f2b8c06848eb
Jens Widell <jl@opera.com>
2015-10-22 13:00:33 +0200

    Submit test results one at a time to the server
    Submitting many test results at the same time risks sending a very
    large request body that the server rejects.  Besides, this traffic
    to the server is not going to be the bottleneck in testing, so there
    is no need to optimize it.

--- client/outgoing.py
+++ client/outgoing.py
@@ -20,36 +20,25 @@ outgoing_dir = os.path.join(configuration["queue-dir"], "outgoing")
 
 try:
     while True:
-        data = {}
-
         with utils.locked_directory(configuration["queue-dir"]):
             filenames = [filename for filename in os.listdir(outgoing_dir)
                          if filename.endswith(".json")]
 
-            for filename in filenames:
-                outgoing_filename = os.path.join(outgoing_dir, filename)
-                with open(outgoing_filename) as outgoing_file:
-                    new_test = json.load(outgoing_file)
-                review_id = filename.partition(":")[0]
-                if review_id not in data:
-                    data[review_id] = []
-                for test in data[review_id]:
-                    if test["key"] == new_test["key"]:
-                        for instance_id, instance_data in new_test["result"].data():
-                            test["result"][instance_id] = instance_data
-                        break
-                else:
-                    data[review_id].append(new_test)
+        if filenames:
+            filename = filenames.pop(0)
 
-        if data:
-            logger.debug("Submitting results...")
+            outgoing_filename = os.path.join(outgoing_dir, filename)
+            with open(outgoing_filename) as outgoing_file:
+                new_test = json.load(outgoing_file)
+            review_id = filename.partition(":")[0]
 
-            critic.operation("CriticTester/submit", data=data)
+            critic.operation("CriticTester/submit",
+                             data={ review_id: [new_test] })
 
             with utils.locked_directory(configuration["queue-dir"]):
-                for filename in filenames:
-                    os.unlink(os.path.join(outgoing_dir, filename))
-                    logger.debug("Deleted: %s" % filename)
+                os.unlink(os.path.join(outgoing_dir, filename))
+
+            logger.debug("Submitted: %s" % filename)
 
         time.sleep(1)
 except KeyboardInterrupt:
